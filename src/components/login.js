796474bd460b008c9fb2e6a10af967a2269ee2d3
VishnuCris <vishnunmms@gmail.com>
2023-08-10 22:35:59 +0530

    login and signup completed

--- src/components/login.js
+++ src/components/login.js
@@ -1,27 +1,94 @@
 
 import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react'
 
-const logim = () => {
+const Login = () => {
+
+   const [credentials,setCredentials] = useState({'email':'','password':''});
+   
+   const handleOnChange = (e) =>{
+      let obj = {};
+      obj[e.target.name] = e.target.value;
+      setCredentials({...credentials,...obj});
+   }
+
+   const handleLogin = () => {
+      if(! credentials.email){
+         alert('email is mendatary')
+         return;
+      }
+      if(! credentials.password){
+         alert('password is mendatary')
+         return;
+      }
+      let users =  localStorage.getItem('user');
+      if(!users){
+         alert('you are not signed up !!!')
+         return;
+      }
+      users = JSON.parse(users)
+
+      let matchedEmail = ''
+      let matchedPassword = ''
+      for(let user of users){
+         if(user?.email == credentials.email){
+            matchedEmail = credentials.email
+         }
+         if(user.password == credentials.password){
+            matchedPassword = credentials.password
+         }
+         if(matchedEmail && matchedPassword)
+            break;
+      }
+
+      if(! matchedEmail){
+         alert('You are not registered with us !!!');
+         return;
+      }else if(! matchedPassword){
+         alert('Wrong Password');
+         return;
+      }
+      localStorage.setItem('currentUser',credentials.email)
+      window.location.href = "/list"
+   }
+
+   useEffect(() => {
+      localStorage.removeItem('currentUser')
+   },[])
 
 	return(
 	 	 <div id="wrapper">
-           <div class="page-wrapper auth_wrapper">
-              <div class="content-area-wrapper">
-                 <div class="content-wrapper">
-                    <div class="container">
-                       <div class="card products_blc">
-                          <div class="card-body">
-                             <div class="card_content_wrap text-center">
-                                <div class="card_content_wrap text-center">
-                                   <div class="logo_wrap">
+           <div className="page-wrapper auth_wrapper">
+              <div className="content-area-wrapper">
+                 <div className="content-wrapper">
+                    <div className="container">
+                       <div className="card products_blc">
+                          <div className="card-body">
+                             <div className="card_content_wrap text-center">
+                                <div className="card_content_wrap text-center">
+                                   <div className="logo_wrap">
                                       <img src="assets/images/thumbnails/Logo.svg" alt="logo" />
-                                      <h6>Don’t have an account yet?<a class="signUpSpan" href="signup.html"><Link to="/signup">Signup</Link></a></h6>
+                                      <h6>Don’t have an account yet?<a className="signUpSpan" href="signup.html"><Link to="/signup">Signup</Link></a></h6>
                                    </div>
                                    <form>
-                                      <div class="form_wrapper">
-                                         <div class="mb-4"><label for="exampleFormControlInput1" class="form-label label_modify"><span class="mendatary">*</span> Email</label><input type="email" class="form-control input_modify" id="exampleFormControlInput1" name="email" placeholder="demo@demo.com" value="" /></div>
-                                         <div class="mb-4"><label for="exampleFormControlInput2" class="form-label label_modify"> <span class="mendatary">*</span> Password</label><input type="password" class="form-control input_modify" name="password" id="exampleFormControlInput1" placeholder="********" value="" /></div>
-                                         <div class="mb-0 auth_btn"><button type="button" class="theme-btn-primary theme-btn">Sign In</button></div>
+                                      <div className="form_wrapper">
+                                         <div className="mb-4">
+                                             <label for="exampleFormControlInput1" className="form-label label_modify">
+                                                <span className="mendatary">*</span> 
+                                                   Email
+                                             </label>
+                                             <input type="email" className="form-control input_modify" id="exampleFormControlInput1" name="email" placeholder="demo@demo.com" value={credentials.email} onChange={handleOnChange}/>
+                                          </div>
+                                         <div className="mb-4">
+                                             <label for="exampleFormControlInput2" className="form-label label_modify">
+                                                <span className="mendatary">*</span>
+                                                   Password
+                                             </label>
+                                             <input type="password" className="form-control input_modify" name="password" id="exampleFormControlInput1" placeholder="********" value={credentials.password} onChange={handleOnChange}/>
+                                          </div>
+                                         <div className="mb-0 auth_btn">
+                                             <button type="button" className="theme-btn-primary theme-btn" onClick={handleLogin}>Sign In</button>
+                                          </div>
                                       </div>
                                    </form>
                                 </div>
@@ -36,4 +103,4 @@ const logim = () => {
 	)
 }
 
-export default logim
+export default Login
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,29 +1,81 @@
 
 import { Link } from "react-router-dom";
+import { useState } from 'react'
 
-const signup = () => {
+const Signup = () => {
+
+   const [credentials,setCredentials] = useState({'email':'','password':'','confirmpassword':''});
+   
+   const handleOnChange = (e) =>{
+      let obj = {};
+      obj[e.target.name] = e.target.value;
+      setCredentials({...credentials,...obj});
+   }
+
+   const handleSignup = () => {
+      console.log(credentials)
+      if(! credentials.email){
+         alert('email is mendatary')
+         return;
+      }
+      if(! credentials.password){
+         alert('password is mendatary')
+         return;
+      }
+      if(credentials.password != credentials.confirmpassword){
+         alert("Re-type password doesn't matched")
+         return;
+      }
+
+      if(!localStorage.getItem('user')){
+         localStorage.setItem('user',JSON.stringify([{'email':credentials.email,'password':credentials.password}]));
+      }else{
+         let existingUsers = localStorage.getItem('user')
+         if(existingUsers){
+            existingUsers = JSON.parse(existingUsers)
+            let matchedEmail = ''
+            for(let existingUser of existingUsers){
+               if(existingUser?.email == credentials.email){
+                  matchedEmail = credentials.email
+                  break;
+               }
+            }
+
+            if(matchedEmail){
+               alert('You are already registered with us !!!');
+               return;
+            }
+            existingUsers.push({'email':credentials.email,'password':credentials.password})
+            localStorage.removeItem('user')
+            localStorage.setItem('user',JSON.stringify(existingUsers))
+         }
+      }
+      localStorage.setItem('currentUser',credentials.email)
+      
+      window.location.href = 'list'
+   }
 
 	return(
 		<div id="wrapper">
-           <div class="page-wrapper auth_wrapper">
-              <div class="content-area-wrapper">
-                 <div class="content-wrapper">
-                    <div class="container">
-                       <div class="card products_blc">
-                          <div class="card-body">
-                             <div class="card_content_wrap text-center"></div>
-                             <div class="card_content_wrap text-center">
-                                <div class="logo_wrap">
+           <div className="page-wrapper auth_wrapper">
+              <div className="content-area-wrapper">
+                 <div className="content-wrapper">
+                    <div className="container">
+                       <div className="card products_blc">
+                          <div className="card-body">
+                             <div className="card_content_wrap text-center"></div>
+                             <div className="card_content_wrap text-center">
+                                <div className="logo_wrap">
                                    <img src="assets/images/thumbnails/Logo.svg" alt="logo" />
                                    <h6>Create an account</h6>
                                 </div>
                                 <form>
-                                   <div class="form_wrapper">
-                                      <div class="mb-4"><label for="exampleFormControlInput1" class="form-label label_modify"><span class="mendatary">*</span> Email</label><input type="email" name="email" placeholder="demo@gmail.com" class="form-control input_modify" id="exampleFormControlInput1" value="" /></div>
-                                      <div class="mb-4"><label for="exampleFormControlInput1" class="form-label label_modify"> <span class="mendatary">*</span> Password</label><input type="password" placeholder="*****" name="password" class="form-control input_modify" id="exampleFormControlInput2" value="" /></div>
-                                      <div class="mb-4"><label for="exampleFormControlInput1" class="form-label label_modify"> <span class="mendatary">*</span>Confirm Password</label><input type="password" name="confirmpassword" class="form-control input_modify" id="exampleFormControlInput3" placeholder="*****" value="" /></div>
-                                      <div class="mb-0 auth_btn"><button type="button" class="theme-btn-primary theme-btn">Sign Up</button></div>
-                                      <div class="already"> <a href="login.html"><Link to="/login">Already have account</Link></a></div>
+                                   <div className="form_wrapper">
+                                      <div className="mb-4"><label for="exampleFormControlInput1" className="form-label label_modify"><span className="mendatary">*</span> Email</label><input type="email" name="email" placeholder="demo@gmail.com" className="form-control input_modify" id="exampleFormControlInput1" value={credentials.email} onChange={handleOnChange}/></div>
+                                      <div className="mb-4"><label for="exampleFormControlInput1" className="form-label label_modify"> <span className="mendatary">*</span> Password</label><input type="password" placeholder="*****" name="password" className="form-control input_modify" id="exampleFormControlInput2" value={credentials.password} onChange={handleOnChange}/></div>
+                                      <div className="mb-4"><label for="exampleFormControlInput1" className="form-label label_modify"> <span className="mendatary">*</span>Confirm Password</label><input type="password" name="confirmpassword" className="form-control input_modify" id="exampleFormControlInput3" placeholder="*****" value={credentials.confirmpassword} onChange={handleOnChange}/></div>
+                                      <div className="mb-0 auth_btn"><button type="button" className="theme-btn-primary theme-btn" onClick={handleSignup}>Sign Up</button></div>
+                                      <div className="already"> <a href="login.html"><Link to="/login">Already have account</Link></a></div>
                                    </div>
                                 </form>
                              </div>
@@ -37,4 +89,4 @@ const signup = () => {
 	)
 }
 
-export default signup
+export default Signup
